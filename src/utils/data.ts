3a730b687362c58a932653ed2f20a56b3daf1840
iamthesaint <stephenie2@me.com>
2024-10-08 02:02:25 -0400

    had a little fun with seed data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,157 +1,289 @@
-import { ObjectId } from 'bson';
+import { ObjectId } from "bson";
+import moment from "moment";
 
-// seed user data
+const getRandomDate = () => {
+    const randomNumberOfDays = Math.floor(Math.random() * 5) + 1;
+    const currentDate = new Date();
+    const randomDate = new Date(
+        currentDate.setDate(currentDate.getDate() - randomNumberOfDays)
+    );
+    return randomDate;
+};
+
 const dbUserData = [
-    {
-        _id: new ObjectId(),
-        username: 'user1',
-        email: 'user1@example.com',
-        thoughts: [] as ObjectId[],
-        friends: [] as ObjectId[]
-    },
-    {
-        _id: new ObjectId(),
-        username: 'user2',
-        email: 'user2@example.com',
-        thoughts: [],
-        friends: []
-    },
-    {
-        _id: new ObjectId(),
-        username: 'user3',
-        email: 'user3@example.com',
-        thoughts: [],
-        friends: []
-    },
-    {
-        _id: new ObjectId(),
-        username: 'user4',
-        email: 'user4@example.com',
-        thoughts: [],
-        friends: []
-    },
-    {
-        _id: new ObjectId(),
-        username: 'user5',
-        email: 'user5@example.com',
-        thoughts: [],
-        friends: []
-    }
+  {
+    _id: new ObjectId(),
+    username: "naruto_uzumaki",
+    email: "naruto@konohagakure.com",
+    thoughts: [] as ObjectId[],
+    friends: [] as ObjectId[],
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    _id: new ObjectId(),
+    username: "saitama1p",
+    email: "saitama@heroassociation.com",
+    thoughts: [],
+    friends: [],
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    _id: new ObjectId(),
+    username: "light_yagami",
+    email: "light@deathnote.com",
+    thoughts: [],
+    friends: [],
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    _id: new ObjectId(),
+    username: "levi_ackerman",
+    email: "levi@surveycorps.com",
+    thoughts: [],
+    friends: [],
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    _id: new ObjectId(),
+    username: "satoru",
+    email: "satoru.gojo@jujutsuhigh.com",
+    thoughts: [],
+    friends: [],
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    _id: new ObjectId(),
+    username: "ichigo-kurosaki",
+    email: "ichi@soulsociety.com",
+    thoughts: [],
+    friends: [],
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    _id: new ObjectId(),
+    username: "nezuko",
+    email: "nezkam@newdemons.com",
+    thoughts: [],
+    friends: [],
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    _id: new ObjectId(),
+    username: "killua",
+    email: "kill_hunter@hunterassociation.com",
+    thoughts: [],
+    friends: [],
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    _id: new ObjectId(),
+    username: "byakuya",
+    email: "bkuchiki@kuchikiclan.com",
+    thoughts: [],
+    friends: [],
+    createdAt: new Date(getRandomDate()),
+  },
 ];
-// seed thought data
+
 const dbThoughtData = [
-    {
-        _id: new ObjectId(),
-        thoughtText: 'This is my first thought!',
-        username: 'user1',
-        reactions: [] as { reactionId: ObjectId, reactionBody: string, username: string }[]
-    },
-    {
-        _id: new ObjectId(),
-        thoughtText: 'Loving the weather today!',
-        username: 'user2',
-        reactions: []
-    },
-    {
-        _id: new ObjectId(),
-        thoughtText: 'Can\'t wait for the weekend!',
-        username: 'user3',
-        reactions: []
-    },
-    {
-        _id: new ObjectId(),
-        thoughtText: 'Just finished a great book!',
-        username: 'user4',
-        reactions: []
-    },
-    {
-        _id: new ObjectId(),
-        thoughtText: 'Had an amazing workout today!',
-        username: 'user5',
-        reactions: []
-    },
-    {
-        _id: new ObjectId(),
-        thoughtText: 'Learning new things every day!',
-        username: 'user1',
-        reactions: []
-    },
-    {
-        _id: new ObjectId(),
-        thoughtText: 'Enjoying a nice cup of coffee!',
-        username: 'user2',
-        reactions: []
-    }
+  {
+    _id: new ObjectId(),
+    thoughtText:
+      "I’ll never go back on my word! That’s my ninja way! I will be the next Hokage!",
+    username: "naruto_uzumaki",
+    reactions: [] as {
+      reactionId: ObjectId;
+      reactionBody: string;
+      username: string;
+      createdAt: Date;
+    }[],
+    createdAt: new Date(new Date().setDate(new Date().getDate() - 10)),
+  },
+  {
+    _id: new ObjectId(),
+    thoughtText: "I’m just a hero for fun. Sometimes, it’s boring. 😴",
+    username: "saitama",
+    reactions: [],
+    createdAt: new Date(new Date().setDate(new Date().getDate() - 5)),
+  },
+  {
+    _id: new ObjectId(),
+    thoughtText: `I'll do anything for my friends. I'll protect them no matter what!`,
+    username: "killua",
+    reactions: [],
+    createdAt: new Date(new Date().setDate(new Date().getDate() - 3)),
+  },
+  {
+    _id: new ObjectId(),
+    thoughtText:
+      "The lesson here is that I’m stronger than you. It’s simple, really.",
+    username: "levi_ackerman",
+    reactions: [],
+    createdAt: new Date(new Date().setDate(new Date().getDate() - 2)),
+  },
+  {
+    _id: new ObjectId(),
+    thoughtText:
+      "If a miracle only happens once, then what is it called the second time?",
+    username: "ichigo-kurosaki",
+    reactions: [],
+    createdAt: new Date(new Date().setDate(new Date().getDate() - 1)),
+  },
+  {
+    _id: new ObjectId(),
+    thoughtText:
+      "A bond is like a pointillist painting. In order to see it in its entirety, you have to take a step back.",
+    username: "byakuya",
+    reactions: [],
+    createdAt: new Date(new Date().setDate(new Date().getDate() - 1)),
+  },
+  {
+    _id: new ObjectId(),
+    thoughtText:
+      "This bamboo muzzle makes it impossible to speak, but I can still post my thoughts here!",
+    username: "nezuko",
+    reactions: [],
+    createdAt: new Date(new Date().setDate(new Date().getDate() - 2)),
+  },
+  {
+    _id: new ObjectId(),
+    thoughtText: "Love is the most twisted curse of all.",
+    username: "satoru",
+    reactions: [],
+    createdAt: new Date(new Date().setDate(new Date().getDate() - 1)),
+  },
+  {
+    _id: new ObjectId(),
+    thoughtText: "I am the god of the new world. I am Kira.",
+    username: "light_yagami",
+    reactions: [],
+    createdAt: new Date(new Date().setDate(new Date().getDate() - 4)),
+  },
 ];
-// seed reaction data
+
 const dbReactionData = [
-    {
-        reactionId: new ObjectId(),
-        reactionBody: 'Great thought!',
-        username: 'user2'
-    },
-    {
-        reactionId: new ObjectId(),
-        reactionBody: 'Totally agree!',
-        username: 'user3'
-    },
-    {
-        reactionId: new ObjectId(),
-        reactionBody: 'Interesting perspective!',
-        username: 'user1'
-    },
-    {
-        reactionId: new ObjectId(),
-        reactionBody: 'Well said!',
-        username: 'user4'
-    },
-    {
-        reactionId: new ObjectId(),
-        reactionBody: 'I feel the same way!',
-        username: 'user5'
-    },
-    {
-        reactionId: new ObjectId(),
-        reactionBody: 'Nice!',
-        username: 'user1'
-    },
-    {
-        reactionId: new ObjectId(),
-        reactionBody: 'Couldn\'t agree more!',
-        username: 'user3'
-    }
+  {
+    reactionId: new ObjectId(),
+    reactionBody: "Believe it!",
+    username: "naruto_uzumaki",
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    reactionId: new ObjectId(),
+    reactionBody: "Awesome!",
+    username: "killua",
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    reactionId: new ObjectId(),
+    reactionBody: "I can’t believe what I just saw!",
+    username: "ichigo-kurosaki",
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    reactionId: new ObjectId(),
+    reactionBody: "A true hero doesn’t need to prove himself.",
+    username: "levi_ackerman",
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    reactionId: new ObjectId(),
+    reactionBody: "I am here!",
+    username: "saitama",
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    reactionId: new ObjectId(),
+    reactionBody: "I can see the ending!",
+    username: "nezuko",
+    createdAt: new Date(getRandomDate()),
+  },
+  {
+    reactionId: new ObjectId(),
+    reactionBody: "I am justice!",
+    username: "light_yagami",
+    createdAt: new Date(getRandomDate()),
+},
+  {
+    reactionId: new ObjectId(),
+    reactionBody: `I know what's going to happen next.`,
+    username: "satoru",
+    createdAt: new Date(getRandomDate()),
+},
+  {
+    reactionId: new ObjectId(),
+    reactionBody: "I am the most powerful!",
+    username: "byakuya",
+    createdAt: new Date(getRandomDate()),
+},
+  {
+    reactionId: new ObjectId(),
+    reactionBody: "I am the fastest!",
+    username: "saitama",
+    createdAt: new Date(getRandomDate()),
+},
+  {
+    reactionId: new ObjectId(),
+    reactionBody: "I am hokage!",
+    username: "naruto_uzumaki",
+    createdAt: new Date(getRandomDate()),
+},
 ];
 
 // associate reactions with thoughts
-dbThoughtData[0].reactions.push(dbReactionData[0], dbReactionData[1]);
-dbThoughtData[1].reactions.push(dbReactionData[2]);
-dbThoughtData[2].reactions.push(dbReactionData[3]);
-dbThoughtData[3].reactions.push(dbReactionData[4]);
-dbThoughtData[4].reactions.push(dbReactionData[5]);
-dbThoughtData[5].reactions.push(dbReactionData[6]);
+dbThoughtData[0].reactions.push(dbReactionData[0]);
+dbThoughtData[1].reactions.push(dbReactionData[4]);
+dbThoughtData[2].reactions.push(dbReactionData[1]);
+dbThoughtData[3].reactions.push(dbReactionData[3]);
+dbThoughtData[4].reactions.push(dbReactionData[2]);
+dbThoughtData[5].reactions.push(dbReactionData[9]);
+dbThoughtData[6].reactions.push(dbReactionData[5]);
+dbThoughtData[7].reactions.push(dbReactionData[8]);
+dbThoughtData[8].reactions.push(dbReactionData[6]);
 
 // get a random item given an array
 export const getRandom = (arr: any[]) => {
-    const randIndex = Math.floor(Math.random() * arr.length);
-    return arr[randIndex];
+  const randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
 };
+
 // get a random user
 export const getRandomUser = () => getRandom(dbUserData);
 
 // add random friends to a user
-export const addRandomFriends = (user: { _id: ObjectId, friends: ObjectId[] }) => {
-    const randomUser = getRandomUser();
-    if (randomUser._id !== user._id && !user.friends.includes(randomUser._id)) {
-        user.friends.push(randomUser._id);
-    }
-    else {
-        addRandomFriends(user);
-    }
+export const addRandomFriends = (user: {
+  _id: ObjectId;
+  friends: ObjectId[];
+}) => {
+  const randomUser = getRandomUser();
+  if (randomUser._id !== user._id && !user.friends.includes(randomUser._id)) {
+    user.friends.push(randomUser._id);
+  }
 };
 
-// log the data
-console.table(dbUserData);
-console.table(dbThoughtData);
-console.info('Seeding Complete! 🌱');
+// add random friends to each user
+dbUserData.forEach(addRandomFriends);
+
+console.table(
+  dbUserData.map((user) => ({
+    ...user,
+    createdAt: user.createdAt
+      ? moment(user.createdAt).format("MMMM Do YYYY, h:mm:ss a")
+      : null,
+  }))
+);
+console.table(
+  dbThoughtData.map((thought) => ({
+    ...thought,
+    createdAt: moment(thought.createdAt).format("MMMM Do YYYY, h:mm:ss a"),
+    reactions: thought.reactions.map((reaction) => ({
+      ...reaction,
+      createdAt: moment(reaction.createdAt).format("MMMM Do YYYY, h:mm:ss a"),
+    })),
+  }))
+);
+
+console.table(dbReactionData);
+console.info("Seeding Complete! 🌱");
 
 export { dbUserData, dbThoughtData, dbReactionData };
